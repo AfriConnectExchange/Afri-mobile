49f2c1feab6187761a3eea6723af3c6a2900de12
africonnectorg <africonnectorg@gmail.com>
2025-10-21 19:28:44 +0000

    added padding to the top

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -121,7 +121,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingHorizontal: 15,
-    paddingTop: 40,
+    paddingTop: 50,
     paddingBottom: 10,
     backgroundColor: 'white',
   },
