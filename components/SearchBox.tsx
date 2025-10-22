85c79678cdfe4fb62e2a30901a04a72e69299844
africonnectorg <africonnectorg@gmail.com>
2025-10-22 05:47:45 +0000

    updated

--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -1,15 +1,5 @@
-import { useSearchBox } from 'react-instantsearch-core';
 import { Input } from './ui/input';
 
-export function SearchBox(props: any) {
-  const { query, refine } = useSearchBox(props);
-
-  return (
-    <Input
-      placeholder="Search products..."
-      value={query}
-      onChangeText={refine}
-      autoFocus
-    />
-  );
+export function SearchBox() {
+  return <Input placeholder="Search products..." />;
 }
