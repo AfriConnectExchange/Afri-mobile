ec18217668aa20fd62c0bab391458bb106934c7a
africonnectorg <africonnectorg@gmail.com>
2025-10-22 05:45:53 +0000

    Changes before Firebase Studio auto-run

--- app/(app)/search.tsx
+++ app/(app)/search.tsx
@@ -1,16 +1,9 @@
-import { searchClient } from '@/lib/algolia';
 import { View, Text } from 'react-native';
-import { InstantSearch } from 'react-instantsearch-core';
-import { SearchBox } from '@/components/SearchBox';
-import { InfiniteHits } from '@/components/InfiniteHits';
 
 export default function SearchScreen() {
   return (
     <View style={{ flex: 1, paddingTop: 60, paddingHorizontal: 15 }}>
-      <InstantSearch searchClient={searchClient} indexName="products">
-        <SearchBox />
-        <InfiniteHits />
-      </InstantSearch>
+      <Text>Search screen</Text>
     </View>
   );
 }
